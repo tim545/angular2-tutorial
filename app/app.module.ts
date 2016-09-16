9f80ec191f183c1986d1946b41325b787926b96c
Tim <tim.freedman@live.com.au>
2016-09-16 15:40:38 +1000

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { routing } from './app.routing';
+
+import { AppComponent }   from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent }   from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }   from './hero.service';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, routing ],
+  declarations: [
+     AppComponent,
+     HeroesComponent,
+     HeroDetailComponent,
+     DashboardComponent
+   ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
+})
+
+export class AppModule {}
---
+++ app/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'app/dashboard.component.html'
+})
+
+export class DashboardComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetail(hero: Hero): void { /* not implemented yet */}
+}
